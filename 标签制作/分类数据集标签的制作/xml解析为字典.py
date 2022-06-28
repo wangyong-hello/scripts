e187c3491a850a8a4f24be9d8f649ce607736f50
wangyong-hello <62507423+wangyong-hello@users.noreply.github.com>
2022-06-28 13:20:28 +0800

    Create xml解析为字典.py

---
+++ 标签制作/分类数据集标签的制作/xml解析为字典.py
@@ -0,0 +1,30 @@
+from lxml import etree
+xml_path=r"C:\Users\WangYong\Desktop\test.xml"
+with open(xml_path) as fid:
+    xml_str = fid.read()
+    xml = etree.fromstring(xml_str)
+
+def parse_xml_to_dict(xml):
+        """
+        将xml文件解析成字典形式，参考tensorflow的recursive_parse_xml_to_dict
+        Args:
+            xml: xml tree obtained by parsing XML file contents using lxml.etree
+
+        Returns:
+            Python dictionary holding XML contents.
+        """
+        if len(xml) == 0:  # 遍历到底层，直接返回tag对应的信息
+            return {xml.tag: xml.text}
+
+        result = {}
+        for child in xml:
+            child_result = parse_xml_to_dict(child)  # 递归遍历标签信息
+            if child.tag != 'object':
+                result[child.tag] = child_result[child.tag]
+            else:
+                if child.tag not in result:  # 因为object可能有多个，所以需要放入列表里
+                    result[child.tag] = []
+                result[child.tag].append(child_result[child.tag])
+        return {xml.tag: result}
+a=parse_xml_to_dict(xml)
+print(a)
